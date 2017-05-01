4f8192aed3a9f903bc259affb0e5c208fc1881ed
Evan Griffiths <griffithse@gmail.com>
2017-05-01 11:30:11 -0400

    work

--- git.py
+++ git.py
@@ -269,12 +269,10 @@ def get_circle_artifacts(owner, repo, ref, GET):
 def _prepare_artifacts(list, base, circle_token):
     '''
     '''
-    artifacts = {relpath(a['pretty_path'], base): '{}?circle-token={}'.format(a['url'], circle_token)
+    artifacts = {relpath('/' + a['pretty_path'], base): '{}?circle-token={}'.format(a['url'], circle_token)
                  for a in list}
 
     getLogger('precog').warning('firstpath {}'.format(list[0]['pretty_path']))
-    getLogger('precog').warning('relpath1 {}'.format(relpath(list[0]['pretty_path'], base)))
-    getLogger('precog').warning('relpath2 {}'.format(relpath('/' + list[0]['pretty_path'], base)))
 
     getLogger('precog').warning('artifacts {}'.format(next(iter(artifacts))))
     getLogger('precog').warning('PRECOG_TARBALL_NAME {}'.format(PRECOG_TARBALL_NAME))
